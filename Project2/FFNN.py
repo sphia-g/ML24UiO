1770361f07894432d9b2fd471d21a7957c4f012b
Minapw <miinapw@gmail.com>
2024-11-11 13:25:34 +0100

    removed unecessary code

--- Project2/FFNN.py
+++ Project2/FFNN.py
@@ -170,38 +170,3 @@ def backpropagation_batch(inputs, layers, activation_funcs, target, activation_d
         layer_grads[i] = (dC_dW, dC_db)
 
     return layer_grads
-
-"""
-def backpropagation_batch(
-    inputs, layers, activation_funcs, target, activation_ders, cost_der=mse_der
-):
-    layer_inputs, zs, predict = feed_forward_saver_batch(inputs, layers, activation_funcs)
-
-    layer_grads = [() for layer in layers]
-
-    # We loop over the layers, from the last to the first
-    for i in reversed(range(len(layers))):
-        layer_input, z, activation_der = layer_inputs[i], zs[i], activation_ders[i]
-
-        if i == len(layers) - 1:
-            # For last layer we use cost derivative as dC_da(L) can be computed directly
-            dC_da = cost_der(predict, target)
-        
-        else:
-            # For other layers we build on previous z derivative, as dC_da(i) = dC_dz(i+1) * dz(i+1)_da(i)
-            (W, b) = layers[i + 1]
-            dC_da = dC_dz @ W.T
-        
-        dC_dz = dC_da.T * activation_der(z)
-        dC_dW = np.outer(layer_input, dC_dz)
-        dC_db = dC_dz 
-           
-        #print(dC_da)
-        #print(dC_dz)
-        #print(dC_dW)
-        #print(dC_db)
-
-        layer_grads[i] = (dC_dW, dC_db)
-
-    return layer_grads
-"""
